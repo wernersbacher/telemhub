dfddc1d8687e92940314ddf8eda5481f360d7b28
Markus Wernersbach <wernersbacher@gmail.com>
2022-03-01 13:35:02 +0100

    notification bug fix

--- routes/helpers/extensions.py
+++ routes/helpers/extensions.py
@@ -11,7 +11,10 @@ from models.models import Notification
 def render_template_extra(template_name_or_list: t.Union[str, t.List[str]], **context: t.Any) -> str:
     """ extends render_template to pass arguments which have to be callled ANYTIME"""
     # get user notifications
-    notif_count = db.session.query(Notification).filter(
-        and_(Notification.owner == current_user, Notification.read == False)).count()
+    if current_user.is_authenticated:
+        notif_count = db.session.query(Notification).filter(
+            and_(Notification.owner == current_user, Notification.read == False)).count()
+    else:
+        notif_count = 0
 
     return render_template(template_name_or_list, notif_count=notif_count, **context)
